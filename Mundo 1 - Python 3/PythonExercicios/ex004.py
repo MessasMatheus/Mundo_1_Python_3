ca0f3e8d8ea338b4742fb19d15a372e13d7e586e
MessasM4 <messas.matheus@gmail.com>
2023-07-04 18:12:39 -0300

    Curso de Pyton 3

--- Mundo 1 - Python 3/PythonExercicios/ex004.py
+++ Mundo 1 - Python 3/PythonExercicios/ex004.py
@@ -1,1 +1,1 @@
-#Faça um programa que leia algo pelo teclado e mostre o seu tipo primitivo e todas as informações possiveis sobre ele.
+#Faça um programa que leia algo pelo teclado e mostre o seu tipo primitivo e todas as informações possíveis sobre ele.
